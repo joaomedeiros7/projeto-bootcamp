aba96c4787aa49bb86967990ed3027a40f4d9d82
joaomedeiros7 <joao.zambrano@ai.infnet.edu.br>
2023-04-09 14:57:48 -0300

    use state

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { AppBar } from "./components/AppBar";
 import { Home } from "./routes/Home";
 import { CreateNotepad } from "./routes/CreateNotePad";
+import { ViewNotepad } from "./routes/ViewNotepad";
 
 function App() {
   return (
@@ -11,8 +12,16 @@ function App() {
       <div>
         <AppBar />
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/criar-notepad" element={<CreateNotepad />} />
+          <Route path="/publicacoes/:id" element={<ViewNotepad />} />
+        </Routes>
+
         <CreateNotepad />
       </div>
     </BrowserRouter>
   );
 }
+
+export default App;
