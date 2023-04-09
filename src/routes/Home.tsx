ff17b389febb3f7c6c3bf88bc58029ed77b924da
joaomedeiros7 <joao.zambrano@ai.infnet.edu.br>
2023-04-09 19:09:45 -0300

    axios

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,36 @@
-import { NotepadList } from "../components/NotepadList";
+import { useState, useEffect } from "react";
+import { NotepadList, Notepad } from "../components/NotepadList";
+
+async function pegaNotepads() {
+  const res = await fetch("https://webservices.jumpingcrab.com/notepads", {
+    method: "get",
+  });
+  const notepads = await res.json();
+  return notepads;
+}
+const initialNotepads: Notepad[] = [];
 
 export function Home() {
+  const [notepads, setNotepads] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const textoCarregando = isLoading ? "careggando..." : "";
+
+  useEffect(() => {
+    setIsLoading(true);
+    pegaNotepads().then((notepads) => {
+      setNotepads(notepads);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    alert("isLoading é igual a: ${isLoading}");
+  }, [isLoading]);
+
   return (
     <div>
-      <NotepadList />
+      <div>{textoCarregando}</div>
+      <NotepadList notepads={notepads} />
     </div>
   );
 }
